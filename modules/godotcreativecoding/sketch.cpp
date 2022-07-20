bc7081f6a7179fe1a70b0024137eb848ad517ad6
jcmonkeydev <dontventonme@gmail.com>
2022-07-20 17:47:59 -0500

    added lines

--- modules/godotcreativecoding/sketch.cpp
+++ modules/godotcreativecoding/sketch.cpp
@@ -1,71 +1,123 @@
 #include "sketch.h"
 
-    void Sketch::_bind_methods(){
-        ClassDB::bind_method(D_METHOD("point"), &Sketch::point);
-        ClassDB::bind_method(D_METHOD("circle"), &Sketch::circle);
-        ClassDB::bind_method(D_METHOD("get_rid"), &Sketch::get_rid);
-        ClassDB::bind_method(D_METHOD("clear"), &Sketch::clear);
-        ClassDB::bind_method(D_METHOD("background"), &Sketch::background);
-        ClassDB::bind_method(D_METHOD("draw_on"), &Sketch::draw_on);
-        ClassDB::bind_method(D_METHOD("draw_off"), &Sketch::draw_off);
-        ClassDB::bind_method(D_METHOD("is_drawing"), &Sketch::is_drawing);
-        ClassDB::bind_method(D_METHOD("size"), &Sketch::size);
-        ClassDB::bind_method(D_METHOD("screen_width"), &Sketch::screen_width);
-        ClassDB::bind_method(D_METHOD("screen_height"), &Sketch::screen_height);
-    }
+void Sketch::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("point", "pos", "color"), &Sketch::point);
+	ClassDB::bind_method(D_METHOD("circle", "pos", "size", "color"), &Sketch::circle);
+	ClassDB::bind_method(D_METHOD("line", "pos1", "pos2", "color", "width"), &Sketch::line);
+	ClassDB::bind_method(D_METHOD("get_rid"), &Sketch::get_rid);
+	ClassDB::bind_method(D_METHOD("clear"), &Sketch::clear);
+	ClassDB::bind_method(D_METHOD("background"), &Sketch::background);
+	ClassDB::bind_method(D_METHOD("draw_on"), &Sketch::draw_on);
+	ClassDB::bind_method(D_METHOD("draw_off"), &Sketch::draw_off);
+	ClassDB::bind_method(D_METHOD("is_drawing"), &Sketch::is_drawing);
+	ClassDB::bind_method(D_METHOD("size"), &Sketch::size);
+	ClassDB::bind_method(D_METHOD("screen_width"), &Sketch::screen_width);
+	ClassDB::bind_method(D_METHOD("screen_height"), &Sketch::screen_height);
+}
 
-    void Sketch::size(float x, float y){
-        //ProjectSettings::get_singleton()->set_setting("display/window/size/width",x);
-        //ProjectSettings::get_singleton()->set_setting("display/window/size/height",y);
-        OS::get_singleton()->set_window_size(Size2(x,y));
-        
-        WIDTH = x;
-        HEIGHT = y;
-    }
+void Sketch::size(float x, float y) {
+	//ProjectSettings::get_singleton()->set_setting("display/window/size/width",x);
+	//ProjectSettings::get_singleton()->set_setting("display/window/size/height",y);
+	OS::get_singleton()->set_window_size(Size2(x, y));
 
-    float Sketch::screen_height(){
-        //return ProjectSettings::get_singleton()->get_setting("display/window/size/height");
-        return HEIGHT;
-    }
+	//WIDTH = x;
+	//HEIGHT = y;
+}
 
-    float Sketch::screen_width(){
-        //return ProjectSettings::get_singleton()->get_setting("display/window/size/width"); 
-        return WIDTH;
-    }
+float Sketch::screen_height() {
+	//return ProjectSettings::get_singleton()->get_setting("display/window/size/height");
+	//return HEIGHT;
+	Vector2 size = OS::get_singleton()->get_window_size();
+	return size.y;
+}
 
-    void Sketch::draw_on(){_candraw = true;}
-    void Sketch::draw_off(){ _candraw = false;}
-    bool Sketch::is_drawing(){ return _candraw;}
+float Sketch::screen_width() {
+	//return ProjectSettings::get_singleton()->get_setting("display/window/size/width");
+	//return WIDTH;
+	Vector2 size = OS::get_singleton()->get_window_size();
+	return size.x;
+}
 
-    //void Sketch::_ready(){}
-    //void Sketch::_process(){
-    //    //print_line("updating!!");
-    //   if (is_drawing()){
-    //        
-    //    }
-    //}
+void Sketch::draw_on() {
+	_candraw = true;
+}
+void Sketch::draw_off() {
+	_candraw = false;
+}
+bool Sketch::is_drawing() {
+	return _candraw;
+}
 
-    RID Sketch::get_rid(){
-        return _ci_rid;  
-    }
+//void Sketch::_ready(){}
+//void Sketch::_process(){
+//    //print_line("updating!!");
+//   if (is_drawing()){
+//
+//    }
+//}
 
-    void Sketch::clear(){
-        VS::get_singleton()->canvas_item_clear(get_rid());
-    }
+RID Sketch::get_rid() {
+	return _ci_rid;
+}
 
-    void Sketch::background(Color color){
-        VS::get_singleton()->set_default_clear_color(color);
-    }
+void Sketch::clear() {
+	VS::get_singleton()->canvas_item_clear(get_rid());
+}
 
+void Sketch::background(Color color) {
+	VS::get_singleton()->set_default_clear_color(color);
+}
 
-    void Sketch::circle(Vector2 pos,float size,Color color = Color(255,255,255,255)){
-        if(is_drawing()){
-            VS::get_singleton()->canvas_item_add_circle(get_rid(),pos,size,color);
-        }
-    }
+//--------------------------------------------------------------------------------------
 
-    void Sketch::point(Vector2 pos,Color color = Color(255,255,255,255)){
-        if(is_drawing()){
-            VS::get_singleton()->canvas_item_add_circle(get_rid(),pos,1,color);
-        }
-    }
+//void Sketch::point(float x, float y, Color color = Color(255, 255, 255, 255)) {
+//	if (is_drawing()) {
+//		Vector2 pos = Vector2(x, y);
+//		VS::get_singleton()->canvas_item_add_circle(get_rid(), pos, 1, color);
+//	}
+//}
+
+void Sketch::point(Vector2 pos, Color color = Color(255, 255, 255, 255)) {
+	if (is_drawing()) {
+		VS::get_singleton()->canvas_item_add_circle(get_rid(), pos, 1, color);
+	}
+}
+
+//void Sketch::circle(float x, float y, float size, Color color = Color(255, 255, 255, 255)) {
+//	if (is_drawing()) {
+//		Vector2 pos = Vector2(x, y);
+//		VS::get_singleton()->canvas_item_add_circle(get_rid(), pos, size, color);
+//	}
+//}
+
+void Sketch::circle(Vector2 pos, float size, Color color = Color(255, 255, 255, 255)) {
+	if (is_drawing()) {
+		VS::get_singleton()->canvas_item_add_circle(get_rid(), pos, size, color);
+	}
+}
+
+/*
+void Sketch::line(float x1, float y1, float x2, float y2, Color color, float width) {
+	if (is_drawing()) {
+		Vector2 pos1 = Vector2(x1, y1);
+		Vector2 pos2 = Vector2(x2, y2);
+		VS::get_singleton()->canvas_item_add_line(get_rid(),
+				pos1,
+				pos2,
+				color,
+				width, // width of line
+				true); // antialiasing enabled
+	}
+}
+*/
+void Sketch::line(Vector2 pos1, Vector2 pos2, Color color, float width) {
+	if (is_drawing()) {
+		VS::get_singleton()->canvas_item_add_line(get_rid(),
+				pos1,
+				pos2, color,
+				width, // width of line
+				true); // antialiasing enabled
+	}
+}
+
+//--------------------------------------------------------------------------------------
--- modules/godotcreativecoding/sketch.h
+++ modules/godotcreativecoding/sketch.h
@@ -3,79 +3,83 @@
 #ifndef SKETCH_H
 #define SKETCH_H
 
-#include "scene/2d/node_2d.h"
-#include "string.h"
 #include "core/math/vector2.h"
-#include "servers/visual_server.h"
-#include "servers/visual/visual_server_canvas.h"
-#include "core/project_settings.h"
 #include "core/os/os.h"
+#include "core/project_settings.h"
+#include "scene/2d/node_2d.h"
+#include "servers/visual/visual_server_canvas.h"
+#include "servers/visual_server.h"
+#include "string.h"
 
 class Sketch : public Node2D {
-    GDCLASS(Sketch, Node2D);
- 
-    bool _candraw;
-    
-    RID _ci_rid;
+	GDCLASS(Sketch, Node2D);
+
+	bool _candraw;
+
+	RID _ci_rid;
 
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 public:
+	float WIDTH;
+	float HEIGHT;
 
-    float WIDTH;
-    float HEIGHT;
+	// drawing functions
+	void draw_on();
+	void draw_off();
+	bool is_drawing();
 
-    // drawing functions
-    void draw_on();
-    void draw_off();
-    bool is_drawing();
+	// main RID
+	RID get_rid();
 
-    // main RID
-    RID get_rid();
+	// utility functions
+	void size(float x, float y);
+	void clear();
+	void background(Color color);
+	float screen_width();
+	float screen_height();
 
-    // utility functions
-    void size(float x, float y);
-    void clear();
-    void background(Color color);
-    float screen_width();
-    float screen_height();
+	// shape functions
+	//void point(float x, float y, Color color);
+	void point(Vector2 pos, Color color);
 
-    // shape functions
-    void point(Vector2 pos,Color color);
-    void circle(Vector2 pos,float size,Color color);
+	//void circle(float x, float y, float size, Color color);
+	void circle(Vector2 pos, float size, Color color);
 
-    //void _ready();
-    //void _process();
+	//void line(float x1, float y1, float x2, float y2, Color color, float width);
+	void line(Vector2 pos1, Vector2 pos2, Color color, float width);
 
-    //void draw();
+	//void _ready();
+	//void _process();
 
-    //void _notification(int what){
-    //    switch(what){
-    //        case NOTIFICATION_PROCESS:
-    //            _process();
-    //            break;
-    //        case NOTIFICATION_READY:
-    //            _ready();
-    //            break;
-    //    }
-    //}
+	//void draw();
 
-    Sketch(){
-        // turn drawing on
-        draw_on();
+	//void _notification(int what){
+	//    switch(what){
+	//        case NOTIFICATION_PROCESS:
+	//            _process();
+	//            break;
+	//        case NOTIFICATION_READY:
+	//            _ready();
+	//            break;
+	//    }
+	//}
 
-        //WIDTH = ProjectSettings::get_singleton()->get_setting("display/window/size/width");
-        //HEIGHT = ProjectSettings::get_singleton()->get_setting("display/window/size/height");
+	Sketch() {
+		// turn drawing on
+		draw_on();
 
-        // create the canvas_item canvas
-        _ci_rid = VS::get_singleton()->canvas_item_create();
-        // set the ci_rid parent to our canvas_item
-        VS::get_singleton()->canvas_item_set_parent(get_rid(), get_canvas_item());
-    } 
-    
-    //----------------------------------------
+		//WIDTH = ProjectSettings::get_singleton()->get_setting("display/window/size/width");
+		//HEIGHT = ProjectSettings::get_singleton()->get_setting("display/window/size/height");
 
+		// create the canvas_item canvas
+		_ci_rid = VS::get_singleton()->canvas_item_create();
+		// set the ci_rid parent to our canvas_item
+		VS::get_singleton()->canvas_item_set_parent(get_rid(), get_canvas_item());
+	}
+
+	//----------------------------------------
 };
 
 #endif // SKETCH_H
